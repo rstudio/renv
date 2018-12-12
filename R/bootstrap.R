0c1a0b94b6abe56641e7bcf457322eaf0fb984c2
Kevin Ushey <kevinushey@gmail.com>
2018-12-11 16:44:25 -0800

    bring back bootstrapper

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,4 +1,21 @@
 
 renv_bootstrap <- function(force = FALSE) {
-  # TODO
+
+  # check for existing 'renv' source
+  source <- find.package("renv", quiet = TRUE)
+  if (!file.exists(source))
+    stop("no installation of 'renv' detected locally")
+
+  # check to see if we already have an installation of 'renv' available
+  target <- renv_paths_bootstrap("renv", packageVersion("renv"))
+
+  # TODO: later
+  # if (file.exists(file.path(target, "renv")) && !force)
+  #   return(TRUE)
+
+  # copy the directory
+  unlink(file.path(target, "renv"), recursive = TRUE)
+  ensure_directory(target)
+  file.copy(source, target, recursive = TRUE)
+
 }
