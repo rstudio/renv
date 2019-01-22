aa9abec87fdb1f4365bae1a00621725d36f30e75
Kevin Ushey <kevinushey@gmail.com>
2019-01-22 09:44:20 -0800

    fixup for non-Windows

--- R/download.R
+++ R/download.R
@@ -3,7 +3,7 @@ download <- function(url, destfile = tempfile()) {
 
   # handle local files by just copying the file
   if (grepl("file:", url)) {
-    source <- sub("^file:[/\\]*", "", url)
+    source <- sub("^file:(?://)?", "", url)
     renv_file_copy(source, destfile, overwrite = TRUE)
     return(destfile)
   }
