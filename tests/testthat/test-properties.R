6524cac5d82c60905fafc88fc295561a68a6074e
Kevin Ushey <kevinushey@gmail.com>
2019-02-20 10:46:36 -0800

    test for property files

---
+++ tests/testthat/test-properties.R
@@ -0,0 +1,19 @@
+
+context("Properties")
+
+test_that("properties can be read successfully", {
+
+  data <- '# This is a comment.\nKey: Value'
+
+  path <- tempfile("renv-properties-")
+  writeLines(data, con = path)
+
+  # trim whitespace by default
+  props <- renv_read_properties(path = path)
+  expect_identical(props, list(Key = "Value"))
+
+  # without trimming whitespace
+  props <- renv_read_properties(path = path, trim = FALSE)
+  expect_identical(props, list(Key = " Value"))
+
+})
