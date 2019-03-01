4b2eac89161b01bbba168885ff0a8726a562f846
Kevin Ushey <kevinushey@gmail.com>
2019-02-28 22:51:12 -0800

    added 'clean' + some extra error handling

--- R/clean.R
+++ R/clean.R
@@ -1,4 +1,47 @@
 
+#' Clean a Project
+#'
+#' Clean up a project. The following actions will be performed:
+#'
+#' - Leftover temporary directories in the project library will be removed.
+#'
+#' @inheritParams renv-params
+#'
+#' @export
+clean <- function(project = NULL, confirm = interactive()) {
+  project <- project %||% renv_state$project()
+  renv_clean_library_tempdirs(project, confirm)
+}
+
+renv_clean_library_tempdirs <- function(project, confirm) {
+
+  library <- renv_paths_library(project)
+  children <- list.files(library, full.names = TRUE)
+
+  bad <- grep("/file\\w{12}$", children, value = TRUE)
+  if (empty(bad))
+    return(character())
+
+  if (confirm || renv_verbose()) {
+
+    renv_pretty_print_packages(
+      bad,
+      "The following directories will be removed:",
+    )
+    writeLines("")
+
+    if (!proceed()) {
+      writeLines("Operation aborted.")
+      return(character())
+    }
+
+  }
+
+  unlink(bad, recursive = TRUE)
+
+}
+
+
 # remove user packages in system library
 renv_clean_system_library <- function() {
   packages <- installed.packages(lib.loc = .Library, priority = "NA")
--- R/download.R
+++ R/download.R
@@ -10,15 +10,34 @@ download <- function(url, destfile = tempfile()) {
 
   vmessagef("Retrieving '%s' ...", url)
 
+  # back up a pre-existing file if necessary
+  callback <- renv_file_scoped_backup(destfile)
+  on.exit(callback(), add = TRUE)
+
+  # TODO: if we already have a file on-disk, perhaps we can
+  # check if the file size matches the reported file size
+  # from the server and skip the download in that case?
+
+  # request the download
   before <- Sys.time()
-  status <- download.file(url, destfile, quiet = TRUE, mode = "wb")
+  status <- catch(download.file(url, destfile, quiet = TRUE, mode = "wb"))
   after <- Sys.time()
-  if (status != 0)
+
+  # check for failure
+  if (inherits(status, "error")) {
+    unlink(destfile, recursive = TRUE)
+    stopf("download failed [%s]", conditionMessage(status))
+  }
+
+  if (status != 0L) {
+    unlink(destfile, recursive = TRUE)
     stopf("download failed [error code %i]", status)
+  }
 
   if (!renv_file_exists(destfile))
     stopf("download failed [unknown reason]")
 
+  # everything looks ok: report success
   if (renv_verbose()) {
     size <- structure(file.info(destfile)$size, class = "object_size")
     time <- round(after - before, 1)
