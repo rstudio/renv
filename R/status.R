52d1833614c50c4a684e04a574a1cfd4b2318ba9
Kevin Ushey <kevinushey@gmail.com>
2019-03-01 20:47:31 -0800

    tidy up + improve status text

--- R/status.R
+++ R/status.R
@@ -25,38 +25,51 @@ renv_status <- function(project) {
   }
 
   # compare the lockfile with current state of library
-  old <- renv_lockfile_read(lockpath)
-  new <- snapshot(file = NULL)
-  renv_status_report(old, new)
+  lock <- renv_lockfile_read(lockpath)
+  curr <- snapshot(file = NULL)
+  renv_status_report(lock, curr)
 
 }
 
-renv_status_report <- function(old, new) {
+renv_status_report <- function(lock, curr) {
 
-  actions <- renv_lockfile_diff_packages(old, new)
+  actions <- renv_lockfile_diff_packages(lock, curr)
   if (empty(actions))
     vmessagef("* The project is already synchronized with the lockfile.")
 
   if ("install" %in% actions) {
     msg <- "The following package(s) are installed but not recorded in the lockfile:"
-    renv_pretty_print(msg, new, actions, "install")
+    renv_pretty_print(msg, curr, actions, "install")
     writeLines("Use `renv::snapshot()` to add these packages to your lockfile.")
     writeLines("")
   }
 
   if ("remove" %in% actions) {
     msg <- "The following package(s) are recorded in the lockfile but not installed:"
-    renv_pretty_print(msg, old, actions, "remove")
+    renv_pretty_print(msg, lock, actions, "remove")
     writeLines("Use `renv::restore(actions = \"install\")` to install these packages.")
     writeLines("")
   }
 
   rest <- c("upgrade", "downgrade", "crossgrade")
   if (any(rest %in% actions)) {
-    msg <- "The following package(s) are out of sync:"
-    renv_pretty_print_pair(msg, old, new, actions, rest)
+
+    matches <- actions[actions %in% rest]
+    data <- data.frame(
+      "  Package"          = names(matches),
+      "  Lockfile Version" = extract_chr(lock$R$Package[names(matches)], "Version"),
+      "  Library Version"  = extract_chr(curr$R$Package[names(matches)], "Version"),
+      row.names            = NULL,
+      stringsAsFactors     = FALSE,
+      check.names          = FALSE
+    )
+
+    writeLines("The following package(s) are out of sync:")
+    writeLines("")
+    print(data, row.names = FALSE, indent = "  ")
+    writeLines("")
     writeLines("Use `renv::snapshot()` to save the state of your library to the lockfile.")
-    writeLines("Use `renv::restore()` to save the state of your library to the lockfile.")
+    writeLines("Use `renv::restore()` to restore your library from the state of the lockfile.")
     writeLines("")
   }
 
