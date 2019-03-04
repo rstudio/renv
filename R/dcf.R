352c718084a5e64871ea9d68fb086ade1b6e320b
Kevin Ushey <kevinushey@gmail.com>
2019-03-03 21:39:23 -0800

    check for empty DESCRIPTION

--- R/dcf.R
+++ R/dcf.R
@@ -1,8 +1,16 @@
 
 renv_dcf_read <- function(file, ...) {
+
+  # if the file is empty, then nothing to do (guard against NA
+  # file sizes if for some reason the filesystem / OS doesn't report it)
+  info <- file.info(file, extra_cols = FALSE)
+  if (identical(as.numeric(info$size), 0))
+    return(data.frame())
+
   # older versions of R could mutate LC_CTYPE (without resetting it)
   # when reading DCF files, so be sure to manage that here
   ctype <- Sys.getlocale("LC_CTYPE")
   on.exit(Sys.setlocale("LC_CTYPE", ctype), add = TRUE)
   as.data.frame(read.dcf(file, ...), stringsAsFactors = FALSE)
+
 }
