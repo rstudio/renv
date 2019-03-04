0cb883513a2644042ea1265705c5339e2367c394
Kevin Ushey <kevinushey@gmail.com>
2019-03-04 15:48:34 -0800

    implement modify (#16); update docs

---
+++ R/modify.R
@@ -0,0 +1,36 @@
+
+#' Open the Lockfile for Editing
+#'
+#' Open a project's lockfile (if any) for editing. After edit, if the lockfile
+#' edited is associated with the active project, any state-related changes
+#' (e.g. to \R repositories) will be updated in the current session.
+#'
+#' @inheritParams renv-params
+#'
+#' @export
+modify <- function(project = NULL) {
+  project <- project %||% renv_project()
+  invisible(renv_modify_impl(project))
+}
+
+renv_modify_impl <- function(project) {
+
+  path <- file.path(project, "renv.lock")
+  if (!file.exists(path))
+    stopf("lockfile '%s' does not exist", aliased_path(path))
+
+  utils::file.edit(path)
+
+  if (!path_same(project, renv_project()))
+    return(NULL)
+
+  lockfile <- catch(renv_lockfile_load(project))
+  if (inherits(lockfile, "error")) {
+    warning(lockfile)
+    return(NULL)
+  }
+
+  renv_load_repos(lockfile$R$Repositories)
+  NULL
+
+}
