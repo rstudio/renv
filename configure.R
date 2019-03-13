cda21a704b8b435b07da7003f9b1040527666244
Kevin Ushey <kevinushey@gmail.com>
2019-03-12 20:54:44 -0700

    don't run configure for package builds

--- configure.R
+++ configure.R
@@ -1,18 +1,26 @@
+
 # used to bump the DESCRIPTION verison every time the package is rebuilt,
 # just so that the renv cache entry is invalidated eagerly
-lines <- readLines("DESCRIPTION")
-idx <- grep("^Version:", lines)
-line <- lines[[idx]]
+local({
 
-old <- numeric_version(sub("^Version:\\s+", "", line))
+  if (!file.exists("internal"))
+    return()
 
-parts <- unclass(old)[[1]]
-if (length(parts) < 4) {
-  parts <- c(parts, rep(0, 4 - length(parts)))
-}
+  lines <- readLines("DESCRIPTION")
+  idx <- grep("^Version:", lines)
+  line <- lines[[idx]]
 
-parts[[4]] <- parts[[4]] + 1
+  old <- numeric_version(sub("^Version:\\s+", "", line))
 
-new <- paste(paste(parts[1:3], collapse = "."), parts[4], sep = "-")
-lines[[idx]] <- paste("Version:", new)
-writeLines(lines, "DESCRIPTION")
+  parts <- unclass(old)[[1]]
+  if (length(parts) < 4) {
+    parts <- c(parts, rep(0, 4 - length(parts)))
+  }
+
+  parts[[4]] <- parts[[4]] + 1
+
+  new <- paste(paste(parts[1:3], collapse = "."), parts[4], sep = "-")
+  lines[[idx]] <- paste("Version:", new)
+  writeLines(lines, "DESCRIPTION")
+
+})
