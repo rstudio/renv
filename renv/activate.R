4b25df4d25245b8adc27e6fb544a638252e53002
Kevin Ushey <kevinushey@gmail.com>
2019-03-13 10:19:09 -0700

    some more work around Python envs

--- renv/activate.R
+++ renv/activate.R
@@ -2,7 +2,7 @@
 local({
 
   # requested version of renv
-  version <- "0.0.1-1066"
+  version <- "0.0.1-1090"
 
   # try to find a path where renv might be installed
   prefix <- file.path(R.version$platform, getRversion()[1, 1:2])
