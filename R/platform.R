dbf2d3266fe1284d87f137bc95753f5300348098
Kevin Ushey <kevinushey@gmail.com>
2019-03-13 09:02:57 -0700

    tidy up

--- R/platform.R
+++ R/platform.R
@@ -1,16 +1,12 @@
 
-renv_platform_sysname_is <- function(name) {
-  Sys.info()[["sysname"]] == name
-}
-
 renv_platform_windows <- function() {
-  renv_platform_sysname_is("Windows")
+  renv_global("platform", Sys.info()[["sysname"]]) == "Windows"
 }
 
 renv_platform_macos <- function() {
-  renv_platform_sysname_is("Darwin")
+  renv_global("platform", Sys.info()[["sysname"]]) == "Darwin"
 }
 
 renv_platform_linux <- function() {
-  renv_platform_sysname_is("Linux")
+  renv_global("platform", Sys.info()[["sysname"]]) == "Linux"
 }
