330a20d8febf1f3f810e448fe98bdbfbf8a4b5c3
Kevin Ushey <kevinushey@gmail.com>
2019-03-13 15:58:54 -0700

    only infer dependency on devtools etc. for current project

--- R/dependencies.R
+++ R/dependencies.R
@@ -135,10 +135,14 @@ renv_dependencies_discover_description <- function(path, fields = NULL) {
 
   })
 
-  # attempt to infer dependency on devtools
-  if (any(c("Roxygen", "RoxygenNote", "Remotes") %in% names(dcf))) {
-    devdeps <- renv_dependencies_list(path, c("roxygen2", "devtools"))
-    data[[length(data) + 1]] <- devdeps
+  # attempt to infer dependency on devtools -- note that this is only
+  # relevant if we're analyizing the DESCRIPTION associating with the
+  # current project
+  if (renv_file_same(path, file.path(renv_project(), "DESCRIPTION"))) {
+    if (any(c("Roxygen", "RoxygenNote", "Remotes") %in% names(dcf))) {
+      devdeps <- renv_dependencies_list(path, c("roxygen2", "devtools"))
+      data[[length(data) + 1]] <- devdeps
+    }
   }
 
   bind_list(data)
