f3a53438098a7a87a281f1792aa79641f1560cb0
Kevin Ushey <kevinushey@gmail.com>
2019-03-13 20:12:27 -0700

    more appropriate name

--- R/debug.R
+++ R/debug.R
@@ -1,11 +1,11 @@
 
 # tools for debugging -- enable to turn off 'renv's error handling,
 # so such errors can reach the top level and be more easily managed
-`_renv_debug` <- FALSE
+`_renv_debugging` <- FALSE
 
-renv_debug <- function(value) {
+renv_debugging <- function(value) {
   if (missing(value))
-    `_renv_debug`
+    `_renv_debugging`
   else
-    `_renv_debug` <<- value
+    `_renv_debugging` <<- value
 }
