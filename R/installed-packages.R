75f94dce8c91e52557cb21479242b9a4745900ee
Kevin Ushey <kevinushey@gmail.com>
2019-03-16 23:42:14 -0700

    ensure dependencies explored in private library

--- R/installed-packages.R
+++ R/installed-packages.R
@@ -4,8 +4,13 @@ renv_installed_packages_base <- function() {
   # we can assume that the base set of installed packages won't change during
   # a session, so cache the result of installed.packages()
   renv_global("base.packages", {
-    ip <- installed.packages(lib.loc = .Library, priority = "base")
-    as.data.frame(ip, stringsAsFactors = FALSE)
+    packages <- installed.packages(lib.loc = .Library, priority = "base")
+    as.data.frame(packages, stringsAsFactors = FALSE)
   })
 
+}
+
+renv_installed_packages <- function(lib.loc = NULL, priority = NULL, ...) {
+  packages <- installed.packages(lib.loc = lib.loc, priority = priority, ...)
+  as.data.frame(packages, stringsAsFactors = FALSE)
 }
