934e1beb6ee007d8e4e1771f260694d4ee691904
Kevin Ushey <kevinushey@gmail.com>
2019-03-17 22:33:11 -0700

    fixup for tests

--- R/debug.R
+++ R/debug.R
@@ -1,11 +1,4 @@
 
 renv_debugging <- function() {
-
-  debugging <- Sys.getenv("RENV_DEBUGGING", unset = NA)
-  if (is.na(debugging))
-    return(FALSE)
-
-  parsed <- parse(text = debugging)[[1]]
-  as.logical(parsed)
-
+  getOption("renv.debugging", default = FALSE)
 }
