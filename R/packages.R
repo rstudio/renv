101f5e90a756aa27580a2fa645586f32c512c958
Kevin Ushey <kevinushey@gmail.com>
2019-03-20 12:02:07 -0700

    tweaks to downloader

--- R/packages.R
+++ R/packages.R
@@ -7,3 +7,40 @@ renv_package_description_field <- function(package, field) {
   desc <- renv_description_read(package = package)
   desc[[field]]
 }
+
+renv_package_type <- function(path) {
+
+  info <- file.info(path, extra_cols = FALSE)
+  if (is.na(info$isdir))
+    stopf("no package at path '%s'", aliased_path(path))
+
+  # for directories, assume it's a source package
+  if (info$isdir)
+    return("source")
+
+  # otherwise, guess based on contents of package
+  methods <- list(
+    untar = function(path) untar(tarfile = path, list = TRUE),
+    unzip = function(path) unzip(zipfile = path, list = TRUE)$Name
+  )
+
+  for (method in methods) {
+
+    files <- catch(method(path))
+    if (inherits(files, "error"))
+      next
+
+    if (any(grepl("^[^/]+/INDEX$", files)))
+      return("binary")
+    else
+      return("source")
+
+  }
+
+  # all else fails, assume source
+  fmt <- "failed to determine type of package '%s'; assuming source"
+  warningf(fmt, aliased_path(path))
+
+  "source"
+
+}
