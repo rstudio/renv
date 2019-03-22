d70bdde111ee23fac28a6b450faa8e27b3f431b3
Kevin Ushey <kevinushey@gmail.com>
2019-03-22 05:51:11 -0700

    confirm when upgrading

--- R/upgrade.R
+++ R/upgrade.R
@@ -4,6 +4,7 @@
 #' Upgrade the version of `renv` associated with a project.
 #'
 #' @inheritParams renv-params
+#'
 #' @param remote A 'remote' specification, as understood by the
 #'   [remotes](https://cran.r-project.org/package=remotes) package. (Note that
 #'   not all remote specifications are yet supported by `renv`.) See
@@ -11,13 +12,32 @@
 #'    for more details. When `NULL` (the default), the latest version of `renv`
 #'   available on GitHub is retrieved instead.
 #'
+#' @param confirm Boolean; confirm upgrade before proceeding?
+#'
 #' @export
-upgrade <- function(project = NULL, remote = "rstudio/renv") {
+upgrade <- function(project = NULL,
+                    remote = "rstudio/renv",
+                    confirm = interactive())
+{
+  invisible(renv_upgrade_impl(project, remote, confirm))
+}
+
+renv_upgrade_impl <- function(project, remote, confirm) {
+
   project <- project %||% renv_project()
 
   # conduct a mini-restore for renv itself
   record <- renv_remotes_parse(remote)
   records <- list(renv = record)
+
+  # produce nice messages based on package versions
+  current <- renv_package_version("renv")
+  request <- record$Version
+  if (version_compare(current, request) == 0) {
+    fmt <- "renv [%s] is already installed and active for this project."
+    writef(fmt, current)
+    return(TRUE)
+  }
 
   renv_pretty_print_packages(
     sprintf("[%s] -> [%s]", renv_package_version("renv"), record$Version),
@@ -25,7 +45,7 @@ upgrade <- function(project = NULL, remote = "rstudio/renv") {
     "This project will use the newly-installed version of renv."
   )
 
-  if (!proceed()) {
+  if (confirm && !proceed()) {
     writeLines("Operation aborted.")
     return(FALSE)
   }
