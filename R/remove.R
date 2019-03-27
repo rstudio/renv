8c257cd1ec069a287ac670fcd566d0197dbd4814
Kevin Ushey <kevinushey@gmail.com>
2019-03-26 23:06:41 -0700

    fix typo

--- R/remove.R
+++ R/remove.R
@@ -15,7 +15,7 @@ remove <- function(packages, library = NULL) {
 
   library <- library %||% renv_libpaths_default()
 
-  if (library == renv_paths_library(project = project)) {
+  if (library == renv_paths_library(project = renv_project())) {
     vwritef("* Removing package(s) from project library ...")
   } else {
     fmt <- "* Removing package(s) from library '%s' ..."
