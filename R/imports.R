39caa52ecf152835e46f59d65df7d0bc5507022d
Kevin Ushey <kevinushey@gmail.com>
2019-03-29 14:33:40 -0700

    import zip

--- R/imports.R
+++ R/imports.R
@@ -2,4 +2,5 @@
 #' @importFrom utils available.packages contrib.url download.file file.edit
 #'   head install.packages installed.packages old.packages packageDescription
 #'   read.table remove.packages tail tar untar update.packages unzip URLencode
+#'   zip
 NULL
