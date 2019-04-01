2cca05befd7f655cab3b84b74000158b60e1d6d0
Kevin Ushey <kevinushey@gmail.com>
2019-04-01 09:13:39 -0700

    marginally better JSON parser

--- R/json.R
+++ R/json.R
@@ -1,13 +1,76 @@
 
-# a very primitive JSON reader, giving us just enough to read the things
-# that the GitHub API produces for us. note that the implementation depends
-# on the JSON being 'nicely' formatted, with braces etc. only occurring at
-# the starts / ends of lines
-renv_json_read <- function(file, text = NULL) {
+# a very hacky, yet 100% R, JSON parser
+renv_json_read <- function(file = NULL, text = NULL) {
+
   text <- text %||% readLines(file, warn = FALSE)
-  transformed <- text
-  transformed <- gsub("[[{]$", "list(", transformed)
-  transformed <- gsub("[]}]$", ")", transformed)
-  transformed <- gsub("\":", "\" =", transformed)
-  parse(text = paste(transformed, collapse = "\n"))
+
+  # find strings in the JSON
+  pattern <- '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'
+  locs <- gregexpr(pattern, text)[[1]]
+
+  # if any are found, replace them with placeholders
+  replaced <- text
+  strings <- character()
+  replacements <- character()
+
+  if (!identical(c(locs), -1L)) {
+
+    # get the string values + compute replacements
+    starts <- locs
+    ends <- locs + attr(locs, "match.length") - 1L
+    strings <- substring(text, starts, ends)
+    replacements <- sprintf('"\032%i\032"', seq_along(strings))
+
+    # replace the strings
+    mapply(function(string, replacement) {
+      replaced <<- sub(string, replacement, replaced, fixed = TRUE)
+    }, strings, replacements)
+
+  }
+
+  # transform the JSON into something the R parser understands
+  transformed <- replaced
+  transformed <- gsub("[[{]", "list(", transformed)
+  transformed <- gsub("[]}]", ")", transformed)
+  transformed <- gsub(":", "=", transformed, fixed = TRUE)
+  json <- parse(text = paste(transformed, collapse = "\n"))[[1]]
+
+  # construct map between source strings, replaced strings
+  map <- as.character(parse(text = strings))
+  names(map) <- as.character(parse(text = replacements))
+
+  renv_json_remap(json, map)
+
+}
+
+renv_json_remap <- function(json, map) {
+
+  # fix names
+  nm <- names(json)
+  if (!is.null(nm))
+    names(json) <- map[names(json)]
+
+  # fix values
+  if (is.character(json))
+    return(map[[json]])
+
+  # handle true, false, null
+  if (is.name(json)) {
+    text <- as.character(json)
+    if (text == "true")
+      return(TRUE)
+    else if (text == "false")
+      return(FALSE)
+    else if (text == "null")
+      return(NA)
+  }
+
+  # recurse
+  if (is.recursive(json)) {
+    for (i in seq_along(json))
+      json[[i]] <- renv_json_remap(json[[i]], map)
+  }
+
+  json
+
 }
---
+++ tests/testthat/test-json.R
@@ -0,0 +1,21 @@
+context("JSON")
+
+test_that("sample JSON strings can be read", {
+
+  expect_identical(
+    renv_json_read(text = '[true, false, null]'),
+    quote(list(TRUE, FALSE, NA))
+  )
+
+  expect_identical(
+    renv_json_read(text = '{"{}": "::"}'),
+    quote(list("{}" = "::"))
+  )
+
+  expect_identical(
+    renv_json_read(text = '[{"a": 1.0}, {"b": -1e5}]'),
+    quote(list(list(a = 1.0), list(b = -1E5)))
+  )
+
+
+})
