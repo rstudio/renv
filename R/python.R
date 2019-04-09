4827567f10108d1c88ffa5f9c4d9cb8159b2a464
Kevin Ushey <kevinushey@gmail.com>
2019-04-09 11:22:36 -0700

    refinements to Python integration

--- R/python.R
+++ R/python.R
@@ -1,30 +1,77 @@
 
-renv_python <- function() {
+renv_python <- function(python = NULL) {
 
-  python <- settings$python()
+  python <- python %||% settings$python()
   if (is.null(python))
     return(NULL)
 
+  # TRUE implies use of a local virtual environment
   if (identical(python, TRUE))
     return(renv_python_local_binary())
 
-  python
+  # validate we have a string
+  if (!is.character(python)) {
+    warning("invalid python setting detected")
+    return(NULL)
+  }
+
+  path <- renv_python_resolve(python)
+  if (!file.exists(path)) {
+    fmt <- "requested Python path '%s' does not exist"
+    warningf(fmt, aliased_path(path))
+    return(NULL)
+  }
+
+  path
+
+}
+
+# figure out the appropriate path for the Python executable
+# given a virtual environment, conda environment, direct
+# path to Python, and so on
+renv_python_resolve <- function(python) {
+
+  # if we have slashes in the name, assume this
+  # is the full path to a Python virtualenv or similar
+  if (!grepl("[/\\\\]", python)) {
+    root <- Sys.getenv("WORKON_HOME", unset = "~/.virtualenvs")
+    python <- file.path(root, python)
+  }
+
+  renv_python_exe(python)
+
+}
+
+
+renv_python_local_init <- function() {
+
+  path <- file.path(renv_project(), "renv/python/r-reticulate")
+  if (file.exists(path))
+    return(path)
+
+  vprintf("* Creating local Python virtual environment ... ", appendLF = FALSE)
+  renv_python_virtualenv_create(path)
+  vwritef("Done!")
+  return(path)
 
 }
 
 renv_python_local_binary <- function() {
+  path <- renv_python_local_init()
+  renv_python_exe(path)
+}
 
-  path <- file.path(renv_project(), "renv/python/r-reticulate")
-  if (!file.exists(path)) {
-    vwritef("* Creating Python virtual environment ... ", appendLF = FALSE)
-    renv_python_virtualenv_create(path)
-    vwritef("Done!")
+renv_python_exe <- function(path) {
+
+  if (renv_python_is_virtualenv(path)) {
+    if (renv_platform_windows())
+      file.path(path, "Scripts/python.exe")
+    else
+      file.path(path, "bin/python")
+  } else {
+    # TODO: conda?
+    path
   }
-
-  if (renv_platform_windows())
-    file.path(path, "Scripts/python.exe")
-  else
-    file.path(path, "bin/python")
 
 }
 
@@ -51,6 +98,7 @@ renv_python_active_binary <- function() {
 }
 
 renv_python_version <- function(python) {
+  python <- normalizePath(python)
   cmd <- paste(shQuote(python), "--version 2>&1")
   output <- catch(system(cmd, intern = TRUE))
   space <- regexpr(" ", output, fixed = TRUE)
@@ -87,6 +135,7 @@ renv_python_pip_freeze <- function(project, python) {
     before <- readLines(path, warn = FALSE)
 
   suffix <- "-m pip freeze 2> /dev/null"
+  python <- normalizePath(python)
   command <- paste(shQuote(python), suffix)
   after <- system(command, intern = TRUE)
 
@@ -108,6 +157,7 @@ renv_python_pip_restore <- function(project, python) {
     before <- readLines(path, warn = FALSE)
 
   suffix <- "-m pip freeze 2> /dev/null"
+  python <- normalizePath(python)
   command <- paste(shQuote(python), suffix)
   after <- system(command, intern = TRUE)
 
@@ -132,6 +182,7 @@ renv_python_virtualenv_create <- function(path) {
   version <- renv_python_version(python)
   module <- if (numeric_version(version) > "3.2") "venv" else "virtualenv"
   fmt <- "%s -m %s %s 2>&1"
+  python <- normalizePath(python)
   cmd <- sprintf(fmt, shQuote(python), module, shQuote(path))
   output <- system(cmd, intern = TRUE)
   status <- attr(output, "status") %||% 0L
@@ -143,6 +194,17 @@ renv_python_virtualenv_create <- function(path) {
 }
 
 renv_python_is_virtualenv <- function(python) {
-  paths <- c("activate_this.py", "pyvenv.cfg", "../pyvenv.cfg")
-  any(file.exists(file.path(dirname(python), paths)))
+
+  info <- file.info(python, extra_cols = FALSE)
+  if (identical(info$isdir, FALSE))
+    python <- dirname(python)
+
+  files <- c("pyvenv.cfg", ".Python")
+  for (root in c(python, dirname(python))) {
+    paths <- file.path(root, files)
+    if (any(file.exists(paths)))
+      return(TRUE)
+  }
+
+  FALSE
 }
