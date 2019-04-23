cc64da3eb898dfb03dae3e51c0989bc853dc93e3
Kevin Ushey <kevinushey@gmail.com>
2019-04-23 13:31:07 -0700

    implement own simple 'git' restore

--- R/retrieve.R
+++ R/retrieve.R
@@ -159,14 +159,39 @@ renv_retrieve_gitlab <- function(record) {
 
 renv_retrieve_git <- function(record) {
 
-  remotes <- renv_retrieve_require("remotes", "Git")
+  renv_git_preflight()
 
-  remote <- remotes$git_remote(
-    url = record$RemoteUrl,
-    ref = record$RemoteSha %||% record$RemoteRef
+  package <- renv_tempfile("renv-git-")
+  ensure_directory(package)
+
+  template <- c(
+    "cd \"${DIR}\"",
+    "git init --quiet",
+    "git remote add origin \"${ORIGIN}\"",
+    "git fetch --quiet origin \"${REF}\"",
+    "git reset --quiet --hard FETCH_HEAD"
   )
 
-  renv_retrieve_remote(record, remote, remotes)
+  data <- list(
+    DIR = normalizePath(package),
+    ORIGIN = record$RemoteUrl,
+    REF = record$RemoteSha %||% record$RemoteRef
+  )
+
+  commands <- renv_template_replace(template, data)
+  command <- paste(commands, collapse = " && ")
+  if (renv_platform_windows())
+    command <- paste("cmd.exe /C", command)
+
+  status <- system(command)
+  if (status != 0L) {
+    fmt <- "cannot retrieve package '%s' from '%s' [status code %i]"
+    stopf(fmt, record$Package, record$RemoteUrl, status)
+  }
+
+  url <- paste("file://", package, sep = "")
+  path <- renv_retrieve_path(record)
+  renv_retrieve_package(record, url, path)
 
 }
 
