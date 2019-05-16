e5b9b51cf98625510566613e4a7dee0f7178c2fd
Kevin Ushey <kevinushey@gmail.com>
2019-05-16 10:56:16 -0700

    add missing shQuote()
    Closes https://github.com/rstudio/renv/issues/76.

--- R/files.R
+++ R/files.R
@@ -233,7 +233,7 @@ renv_file_list_impl <- function(path) {
   # 'dir' with the code page set to request UTF-8 encoded paths
   if (renv_platform_windows()) {
     path <- normalizePath(path)
-    command <- paste(comspec(), "/c chcp 65001 && dir /B", path)
+    command <- paste(comspec(), "/c chcp 65001 && dir /B", shQuote(path))
     output <- system(command, intern = TRUE)
     Encoding(output) <- "UTF-8"
     return(output)
