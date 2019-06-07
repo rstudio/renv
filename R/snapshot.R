c9634883e9bdda2f2014c889245e0fc93644fe1e
Kevin Ushey <kevin@rstudio.com>
2019-06-07 13:41:55 -0700

    use warningf helper

--- R/snapshot.R
+++ R/snapshot.R
@@ -219,7 +219,7 @@ renv_snapshot_validate_dependencies <- function(project, lockfile, library, conf
   request  <- bad$Requested
 
   fmt <- "- %s requires %s, but %s will be snapshotted"
-  warning(sprintf(fmt, format(package), format(requires), format(request)))
+  warningf(fmt, format(package), format(requires), format(request))
 
   return(FALSE)
 }
