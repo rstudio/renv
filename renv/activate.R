a58fc62e33f911af72045046a6117d63ae5d16a1
Kevin Ushey <kevinushey@gmail.com>
2019-06-24 14:51:30 -0700

    update renv

--- renv/activate.R
+++ renv/activate.R
@@ -2,7 +2,11 @@
 local({
 
   # the requested version of renv
-  version <- "0.5.0-6"
+  version <- "0.5.0-33"
+
+  # signal that we're loading renv during R startup
+  Sys.setenv("RENV_R_INITIALIZING" = "true")
+  on.exit(Sys.unsetenv("RENV_R_INITIALIZING"), add = TRUE)
 
   # load the 'utils' package eagerly -- this ensures that renv shims, which
   # mask 'utils' packages, will come first on the search path
@@ -23,10 +27,10 @@ local({
   }
 
   # helper for sourcing the user profile (if any)
+  # respect R_PROFILE_USER when sourcing user profile
+  profile <- Sys.getenv("R_PROFILE_USER", unset = "~/.Rprofile")
   source_user_profile <- function() {
 
-    # respect R_PROFILE_USER when sourcing user profile
-    profile <- Sys.getenv("R_PROFILE_USER", unset = "~/.Rprofile")
     if (!file.exists(profile))
       return(FALSE)
 
@@ -48,8 +52,10 @@ local({
 
   }
 
-  # load user profile on exit
-  on.exit(source_user_profile(), add = TRUE)
+  # load user profile now -- needs to happen before renv is loaded,
+  # as otherwise the user profile could clobber the library paths
+  # that renv tries to set up for the project
+  source_user_profile()
 
   # figure out root for renv installation
   default <- switch(
