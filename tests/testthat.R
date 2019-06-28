345fd7f12ead520ec5564a01a18f0b894b79db4d
Kevin Ushey <kevinushey@gmail.com>
2019-06-28 14:47:03 -0700

    add R 3.4

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,7 +5,6 @@ Sys.unsetenv("RENV_TESTS_INITIALIZED")
 Sys.unsetenv("RENV_TESTS_INITIALIZED_TESTTHAT")
 
 renv:::renv_tests_init()
-renv:::renv_tests_report()
 
 Sys.setenv("RENV_TESTS_INITIALIZED_TESTTHAT" = "1")
 
