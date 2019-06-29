11b8324ef30ef6884b171fd0ccd1c9e59aad0232
Kevin Ushey <kevinushey@gmail.com>
2019-06-28 20:27:20 -0700

    use scope helper

--- tests/testthat/test-patches.R
+++ tests/testthat/test-patches.R
@@ -2,14 +2,7 @@
 context("Patches")
 
 test_that("a bad TAR is repaired", {
-
-  # make sure to restore old TAR (e.g. we force 'internal' on AppVeyor)
-  TAR <- Sys.getenv("TAR")
-  on.exit(Sys.setenv(TAR = TAR), add = TRUE)
-
-  badtar <- "/no/such/tar"
-  Sys.setenv(TAR = badtar)
+  renv_scope_envvars(TAR = "/no/such/tar")
   expect_warning(renv_patch_tar())
-  expect_false(Sys.getenv("TAR") == badtar)
-
+  expect_false(Sys.getenv("TAR") == "/no/such/tar")
 })
