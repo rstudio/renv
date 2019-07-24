e6623dde30d38b98b21f735955195ada7021e69d
Kevin Ushey <kevinushey@gmail.com>
2019-07-23 21:52:24 -0700

    alternate system for R_LIBS during install

--- R/R.R
+++ R/R.R
@@ -7,24 +7,12 @@ R <- function() {
 
 r_exec <- function(package, args, label) {
 
-  # pass along environment variables to child process
-  keys <- c("R_ENVIRON", "R_ENVIRON_USER", "R_MAKEVARS", "R_MAKEVARS_USER")
-  vals <- Sys.getenv(keys, unset = NA)
-  envvars <- vals[!is.na(vals)]
-
-  # include R_LIBS
-  envvars <- c(
-    R_LIBS = paste(renv_libpaths_all(), collapse = .Platform$path.sep),
-    envvars
-  )
-
-  # generate env as used by system2
-  env <- paste(names(envvars), shQuote(envvars), sep = "=")
+  # ensure R_LIBS is set
+  rlibs <- paste(renv_libpaths_all(), collapse = .Platform$path.sep)
+  renv_scope_envvars(R_LIBS = rlibs, R_LIBS_USER = "", R_LIBS_SITE = "")
 
   # do the install
-  output <- suppressWarnings(
-    system2(R(), args, stdout = TRUE, stderr = TRUE, env = env)
-  )
+  output <- suppressWarnings(system2(R(), args, stdout = TRUE, stderr = TRUE))
 
   # check for successful install
   status <- attr(output, "status") %||% 0L
@@ -50,6 +38,7 @@ r_cmd_install <- function(package, path, library, ...) {
   library <- normalizePath(library, winslash = "/", mustWork = TRUE)
 
   args <- c(
+    "--vanilla",
     "CMD", "INSTALL", "--preclean",
     r_cmd_install_option(package, "configure.args", TRUE),
     r_cmd_install_option(package, "configure.vars", TRUE),
@@ -66,7 +55,7 @@ r_cmd_install <- function(package, path, library, ...) {
 
 r_cmd_build <- function(package, path, ...) {
   path <- normalizePath(path, winslash = "/", mustWork = TRUE)
-  args <- c("CMD", "build", "--md5", ..., shQuote(path))
+  args <- c("--vanilla", "CMD", "build", "--md5", ..., shQuote(path))
   output <- r_exec(package, args, "build")
   pasted <- paste(output, collapse = "\n")
   pattern <- "[*] building .([a-zA-Z0-9_.-]+)."
