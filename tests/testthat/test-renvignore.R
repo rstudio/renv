31538948aabf756f2d9ce97d3aaf4b61cb0d552d
Kevin Ushey <kevinushey@gmail.com>
2019-08-01 10:16:08 -0700

    add simple test for .renvignore

---
+++ tests/testthat/test-renvignore.R
@@ -0,0 +1,22 @@
+
+test_that(".renvignore ignores files, directories", {
+
+  renv_tests_scope("oatmeal")
+
+  writeLines(c("internal/", "data.R"), con = ".renvignore")
+
+  # data file at root
+  writeLines("library(breakfast)", con = "data.R")
+
+  # internal folder at root
+  dir.create("internal")
+  writeLines("library(breakfast)", con = "internal/script.R")
+
+  # internal subfolder
+  dir.create("scripts/internal", recursive = TRUE)
+  writeLines("library(breakfast)", con = "scripts/internal/script.R")
+
+  deps <- dependencies()
+  expect_setequal(deps$Package, "oatmeal")
+
+})
