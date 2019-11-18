a2c179328c56f32249f3e8f5ddc74971f7d7a03b
Kevin Ushey <kevinushey@gmail.com>
2019-11-18 14:27:45 -0800

    tweak error reporting

--- R/system.R
+++ R/system.R
@@ -1,18 +1,37 @@
 
 renv_system_exec <- function(command, args, action, success = 0L) {
 
+  # suppress warnings as some successful commands
+  # may return a non-zero exit code, whereas R
+  # will always warn on such error codes
   output <- suppressWarnings(
     system2(command, args, stdout = TRUE, stderr = TRUE)
   )
 
+  # extract status code from result
   status <- attr(output, "status") %||% 0L
-  if (!status %in% success) {
-    writeLines(output, con = stderr())
-    fmt <- "error %s [error code %i]"
-    stopf(fmt, action, status)
-  }
 
-  output
+  # if this status matches an expected 'success' code, return output
+  if (status %in% success)
+    return(output)
+
+  # otherwise, notify the user that things went wrong
+  cmdline <- paste(command, paste(args, collapse = " "))
+  underline <- paste(rep.int("=", min(80L, nchar(cmdline))), collapse = "")
+  header <- c(cmdline, underline)
+
+  # truncate output (avoid overwhelming console)
+  body <- if (length(output) > 200L)
+    c(head(output, n = 100L), "< ... >", tail(output, n = 100L))
+  else
+    output
+
+  # write to stderr
+  writeLines(c(header, "", body), con = stderr())
+
+  # throw error
+  fmt <- "error %s [error code %i]"
+  stopf(fmt, action, status)
 
 }
 
