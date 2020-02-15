5148e8a5f8f93020c5125d8503d4660941fb7bd4
Kevin Ushey <kevinushey@gmail.com>
2020-02-15 14:18:41 -0800

    back out tests

--- tests/testthat/test-id.R
+++ tests/testthat/test-id.R
@@ -7,18 +7,3 @@ test_that("we can generate a unique ID", {
   expect_true(nchar(id) == 36L)
   expect_true(grepl("^[a-fA-F0-9-]+$", id))
 })
-
-test_that("projects are assigned a unique ID when created", {
-  skip_on_cran()
-  renv_tests_scope()
-
-  renv::init(bare = TRUE)
-  expect_true(file.exists("renv/project-id"))
-
-  contents <- readLines("renv/project-id")
-  expect_identical(contents, renv_project_id(getwd()))
-
-  id <- renv_project_id(getwd())
-  contents <- readLines("renv/project-id")
-
-})
