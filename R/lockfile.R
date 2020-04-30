4a3134c2c534a605c8913ffff15f2135ca6b883a
Kevin Ushey <kevinushey@gmail.com>
2020-04-30 14:33:11 -0700

    don't mutate RSPM URLs when integration disabled

--- R/lockfile.R
+++ R/lockfile.R
@@ -32,8 +32,10 @@ renv_lockfile_init_r_repos <- function(project) {
   repos[repos == "@CRAN@"] <- "https://cloud.r-project.org"
 
   # remove RSPM bits from URL
-  pattern <- "/__[^_]+__/[^/]+/"
-  repos <- sub(pattern, "/", repos)
+  if (config$rspm.enabled()) {
+    pattern <- "/__[^_]+__/[^/]+/"
+    repos <- sub(pattern, "/", repos)
+  }
 
   # force as list
   repos <- as.list(repos)
