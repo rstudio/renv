2f4b94429e158f00265bfee41690d0eef302dc65
Kevin Ushey <kevinushey@gmail.com>
2020-04-30 09:51:46 -0700

    transform RSPM repository on load (closes #410)

--- R/load.R
+++ R/load.R
@@ -110,6 +110,9 @@ renv_load_r_repos <- function(repos) {
   repos <- sub("/+$", "", repos)
   names(repos) <- nms
 
+  # convert to rspm
+  repos <- renv_rspm_transform(repos)
+
   # set sanitized repos
   options(repos = repos)
 
