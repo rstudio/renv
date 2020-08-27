4134edc10ce0edbc58651590499c9158a5627a8e
Kevin Ushey <kevinushey@gmail.com>
2020-08-27 11:57:28 -0700

    don't stall when querying non-existent repository

--- tests/testthat/test-available-packages.R
+++ tests/testthat/test-available-packages.R
@@ -3,7 +3,13 @@ context("Available Packages")
 
 test_that("renv_available_packages() errs on incorrect repository", {
   skip_on_cran()
-  renv_scope_options(repos = c(CRAN = "https://www.example.com/no/such/repository"))
+
+  renv_scope_options(
+    renv.config.connect.timeout = 0L,
+    renv.config.connect.retry   = 0L,
+    repos = c(CRAN = "https://www.example.com/no/such/repository")
+  )
+
   expect_error(renv_available_packages(type = "source"))
 })
 
