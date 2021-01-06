05028b9e43d4a7851a116f49e93e7671f7730dbe
Kevin Ushey <kevinushey@gmail.com>
2021-01-05 21:26:42 -0800

    missing import

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,6 @@
 
-#' @importFrom utils adist available.packages citation contrib.url download.file
+#' @importFrom utils
+#'   adist available.packages browseURL citation contrib.url download.file
 #'   download.packages file.edit getCRANmirrors head help install.packages
 #'   installed.packages old.packages packageDescription packageVersion
 #'   read.table remove.packages sessionInfo str tail tar toBibtex untar
