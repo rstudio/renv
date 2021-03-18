912827f0d8ef93ebb1a901a85414a92a9f0fd549
Kevin Ushey <kevinushey@gmail.com>
2021-03-18 12:08:37 -0700

    print more diagnostics when preparing for tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,43 @@
 library(testthat)
 library(renv)
 
-if (renv:::renv_tests_supported()) {
+run <- function() {
 
-  renv:::renv_tests_init()
-  renv:::renv_tests_diagnostics()
+  # check whether we can run tests here
+  if (!renv:::renv_tests_supported()) {
+    message("renv does not support running tests on this platform.")
+    return(FALSE)
+  }
 
+  # try to initialize for testing
+  status <- tryCatch(
+    renv:::renv_tests_init(),
+    error = identity
+  )
+
+  if (inherits(status, "error")) {
+    message("renv encountered one or more problems while initializing tests.")
+    message(status$message)
+    return(FALSE)
+  }
+
+  # run test diagnostics
+  status <- tryCatch(
+    renv:::renv_tests_diagnostics(),
+    error = identity
+  )
+
+  if (inherits(status, "error")) {
+    message("renv encountered one or more problems while printing diagnostics.")
+    message(status$message)
+    return(FALSE)
+  }
+
+  # run the tests
   reporter <- renv:::renv_tests_reporter()
   test_check("renv", reporter = reporter)
 
 }
+
+run()
+
