c445112b71eae7835582283894d3b0c253f65953
Roy Storey <kiwiroy@users.noreply.github.com>
2021-09-02 20:40:17 +1200

    test /.singularity.d

--- R/consent.R
+++ R/consent.R
@@ -79,6 +79,7 @@ renv_consent_check <- function() {
     !is.na(Sys.getenv("CI", unset = NA)) ||
     !is.na(Sys.getenv("GITHUB_ACTION", unset = NA)) ||
     !is.na(Sys.getenv("RENV_PATHS_ROOT", unset = NA)) ||
+    file.exists("/.singularity.d") ||
     file.exists("/.dockerenv")
 
   if (consented)
