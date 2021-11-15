7a3df9e362f8c7f124e3f97ef99e49b0b6c5c253
Kevin Ushey <kevinushey@gmail.com>
2021-11-15 11:43:30 -0800

    add missing import

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #' @importFrom utils
 #'   adist available.packages browseURL citation contrib.url download.file
 #'   download.packages file.edit getCRANmirrors head help install.packages
-#'   installed.packages old.packages packageDescription packageVersion
-#'   read.table remove.packages sessionInfo str tail tar toBibtex untar
-#'   update.packages unzip URLencode zip
+#'   installed.packages modifyList old.packages packageDescription
+#'   packageVersion read.table remove.packages sessionInfo str tail tar
+#'   toBibtex untar update.packages unzip URLencode zip
 NULL
