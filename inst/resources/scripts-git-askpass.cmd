fb4615b4d5413158439004a98d3b5f32291e7eb2
Kevin Ushey <kevin@rstudio.com>
2022-04-12 13:42:43 -0700

    tweaks to windows git script

--- inst/resources/scripts-git-askpass.cmd
+++ inst/resources/scripts-git-askpass.cmd
@@ -1,11 +1,14 @@
 @echo off
-
 setlocal EnableExtensions
+
+set "_GIT_PROMPT=%~1"
 
 if defined GIT_USERNAME set "GIT_USER=%GIT_USERNAME%"
 if defined GIT_PASSWORD set "GIT_PASS=%GIT_PASSWORD%"
 
-set "_GIT_PROMPT=%1"
+if not defined GIT_USER set "GIT_USER=nobody"
+if not defined GIT_PASS set "GIT_PASS=nobody"
+
 if /i "%_GIT_PROMPT:~0,8%"=="Username" echo %GIT_USER%
 if /i "%_GIT_PROMPT:~0,8%"=="Password" echo %GIT_PASS%
 
