8aa622167ec464059a12009cb1057e982cfb4c0f
Kevin Ushey <kevinushey@gmail.com>
2022-11-07 21:12:12 -0800

    tweak

--- inst/resources/vendor/renv.R
+++ inst/resources/vendor/renv.R
@@ -3,6 +3,10 @@ renv <- new.env(parent = new.env())
 
 renv$initialize <- function() {
 
+  # set up renv + imports environments
+  attr(renv, "name") <- "embedded:renv"
+  attr(parent.env(renv), "name") <- "imports:renv"
+
   # set up metadata environment
   metadata <- list(
     embedded = TRUE,
@@ -15,10 +19,6 @@ renv$initialize <- function() {
   # get imports
   imports <- ..imports..
 
-  # set up renv + imports environments
-  attr(renv, "name") <- "embedded:renv"
-  attr(parent.env(renv), "name") <- "imports:renv"
-
   # load the imports required by renv
   for (package in names(imports)) {
     namespace <- asNamespace(package)
@@ -29,5 +29,8 @@ renv$initialize <- function() {
   # source renv into the aforementioned environment
   script <- system.file("vendor/renv.R", package = .packageName)
   sys.source(script, envir = renv)
+
+  # remove our initialize method when we're done
+  rm(list = "initialize", envir = renv)
 
 }
