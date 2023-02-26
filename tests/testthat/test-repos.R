06e7693eac29f9e134b878d776024880cc245259
Kevin Ushey <kevinushey@gmail.com>
2023-02-26 12:36:07 -0800

    further refine caching of available packages

--- tests/testthat/test-repos.R
+++ tests/testthat/test-repos.R
@@ -4,6 +4,9 @@ context("Repositories")
 test_that("we can query our local repository during tests", {
 
   expected <- list.files("packages")
+  drop <- if (.Platform$OS.type == "unix") "windowsonly" else "unixonly"
+  expected <- setdiff(expected, drop)
+
   renv_tests_scope()
   ap <- renv_available_packages(type = "source")[[1]]
   expect_setequal(ap$Package, expected)
