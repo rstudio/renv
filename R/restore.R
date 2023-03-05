9eed5ec29d18ed4a77ad1ed89609294c63d34b11
Kevin Ushey <kevinushey@gmail.com>
2023-03-05 13:45:08 -0800

    init bioc in restore

--- R/restore.R
+++ R/restore.R
@@ -106,6 +106,7 @@ restore <- function(project  = NULL,
   # set up Bioconductor version + repositories
   biocversion <- lockfile$Bioconductor$Version
   if (!is.null(biocversion)) {
+    renv_bioconductor_init(library = library)
     biocversion <- package_version(biocversion)
     renv_scope_options(renv.bioconductor.version = biocversion)
   }
