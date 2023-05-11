7cd4a98921b8f277322a3d4e9d9d5e71a0b8b0a7
Hadley Wickham <h.wickham@gmail.com>
2023-05-11 16:08:27 -0500

    Improve tests

--- tests/testthat/test-envvar.R
+++ tests/testthat/test-envvar.R
@@ -4,14 +4,19 @@ test_that("renv_envvar_prepend doesn't duplicate paths", {
   path_string <- function(...) {
     paste(c(...), collapse = .Platform$path.sep)
   }
-
   withr::local_envvar(TESTPATH = path_string("a", "b", "c"))
-  expect_equal(
-    renv_envvar_prepend("TESTPATH", "a"),
-    path_string("a", "b", "c")
-  )
-  expect_equal(
-    renv_envvar_prepend("TESTPATH", "b"),
-    path_string("b", "a", "c")
-  )
+
+  renv_envvar_prepend("TESTPATH", "a")
+  expect_equal(Sys.getenv("TESTPATH"), path_string("a", "b", "c"))
+
+  renv_envvar_prepend("TESTPATH", "b")
+  expect_equal(Sys.getenv("TESTPATH"), path_string("b", "a", "c"))
+})
+
+test_that("renv_envvar_prepend works if var isn't set", {
+
+  renv_envvar_prepend("TESTPATH", "a")
+  defer(Sys.unsetenv("TESTPATH"))
+
+  expect_equal(Sys.getenv("TESTPATH"), "a")
 })
