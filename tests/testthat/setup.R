71f4e947dbba56d6ff9644d1d2f6d586444ac0a7
Hadley Wickham <h.wickham@gmail.com>
2023-05-19 07:59:09 -0500

    Improve error + normalize path

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,9 +1,19 @@
 
 renv_test_scope_setup(testthat::teardown_env())
 
-if (dirname(renv_paths_root()) != tempdir()) {
-  stop(
-    "`renv_paths_root()` not set to temporary directory\n",
-    "* running the tests may mutate system renv state"
-  )
-}
+local({
+  rootdir <- normalizePath(dirname(renv_paths_root()), winslash = "/", mustWork = FALSE)
+  tempdir <- normalizePath(tempdir(), winslash = "/")
+
+  if (rootdir != tempdir) {
+    stopf(
+      paste0(
+        "`renv_paths_root()` ('%s') not set to temporary directory ('%s')\n",
+        "* running the tests may mutate system renv state"
+      ),
+      root,
+      temp
+    )
+  }
+})
+
