5e47ab2f563db1807302ab1864f6593562a1d8e8
Hadley Wickham <h.wickham@gmail.com>
2023-05-25 15:15:28 -0500

    Eliminate simple env var wrappers

--- R/envvar.R
+++ R/envvar.R
@@ -1,25 +1,4 @@
 
-
-renv_envvar_get <- function(key, unset = NA) {
-  Sys.getenv(key, unset = unset)
-}
-
-renv_envvar_set <- function(key, value) {
-
-  # handle NULL / NA values
-  if (is.null(value) || is.na(value))
-    return(Sys.unsetenv(key))
-
-  # otherwise, delegate
-  args <- structure(list(value), names = key)
-  do.call(Sys.setenv, args)
-
-}
-
-renv_envvar_clear <- function(key) {
-  Sys.unsetenv(key)
-}
-
 renv_envvar_path_add <- function(envvar, value, prepend = TRUE) {
 
   old <- Sys.getenv(envvar, unset = "")
