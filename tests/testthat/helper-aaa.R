00e20dbeb5be2bd5142ab8cff1d49ba8828ce604
Hadley Wickham <h.wickham@gmail.com>
2023-05-25 17:13:22 -0500

    Store test repo path in option (#1347)
    * Store test repo path in option
    * Hack fix for test file button

--- tests/testthat/helper-aaa.R
+++ tests/testthat/helper-aaa.R
@@ -1,10 +1,8 @@
 
 teardown_env <- function() {
-  if (interactive()) globalenv() else testthat::teardown_env()
-}
-
-`_renv_tests_repopath` <- renv_scope_tempfile("renv-repos-", envir = teardown_env())
-
-renv_tests_repopath <- function() {
-  `_renv_tests_repopath`
+  if (is.null(renv_namespace_load("testthat")$testthat_env$teardown_env)) {
+    globalenv()
+  } else {
+    testthat::teardown_env()
+  }
 }
