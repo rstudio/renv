cdc22f0c8f5f3189e2133b69763873ad2dcbdfd7
Hadley Wickham <h.wickham@gmail.com>
2023-05-25 07:40:10 -0500

    Eliminate use of renv_global_*

--- R/dependencies.R
+++ R/dependencies.R
@@ -1618,18 +1618,20 @@ renv_dependencies_require <- function(package, type = NULL) {
 
 }
 
+`_renv_dependencies.state` <- NULL
+
 renv_dependencies_state <- function(key = NULL) {
-  state <- renv_global_get("dependencies.state")
+  state <- `_renv_dependencies.state`
   if (is.null(key)) state else state[[key]]
 }
 
 renv_dependencies_begin <- function(root = NULL) {
   state <- env(root = root, scanned = env(), problems = stack())
-  renv_global_set("dependencies.state", state)
+  `_renv_dependencies.state` <<- state
 }
 
 renv_dependencies_end <- function() {
-  renv_global_clear("dependencies.state")
+  `_renv_dependencies.state` <<- NULL
 }
 
 renv_dependencies_error <- function(path, error = NULL, packages = NULL) {
