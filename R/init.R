946d55e484f5bd91e6d5b158d18ea82e226f92e8
Kevin Ushey <kevinushey@gmail.com>
2023-05-25 17:19:56 -0700

    update docs

--- R/init.R
+++ R/init.R
@@ -20,9 +20,19 @@
 #' to do the right thing: it will restore the project library if it's missing,
 #' or otherwise ask you what to do.
 #'
+#' # Repositories
+#'
+#' If the default \R repositories have not already been set, renv will use
+#' the [Posit Public Package Manager](https://packagemanager.posit.co/) CRAN
+#' mirror for package installation. The primary benefit to using this mirror is
+#' that it can provide pre-built binaries for \R packages on a variety of
+#' commonly-used Linux distributions. This behavior can be configured or
+#' disabled if desired -- see the options in [renv::config()] for more details.
+#'
 #' @inherit renv-params
 #'
-#' @param project The project directory. The \R working directory will be
+#' @param project The project directory. When `NULL` (the default), the current
+#'   working directory will be used. The \R working directory will be
 #'   changed to match the requested project directory.
 #'
 #' @param settings A list of [settings] to be used with the newly-initialized
@@ -35,8 +45,8 @@
 #'   to initialize the home directory as a project, to defend against accidental
 #'   mis-usages of `init()`.
 #'
-#' @param repos The \R repositories to be used in this project. By default,
-#'   the active repositories (as determined by `getOption("repos")`) are used.
+#' @param repos The \R repositories to be used in this project.
+#'   See **Repositories** for more details.
 #'
 #' @param bioconductor The version of Bioconductor to be used with this project.
 #'   Setting this may be appropriate if renv is unable to determine that your
