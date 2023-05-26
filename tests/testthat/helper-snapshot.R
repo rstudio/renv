e7f6c8978280c4c4b0661d92f2b9578b47ba0fa8
Hadley Wickham <h.wickham@gmail.com>
2023-05-26 09:41:35 -0500

    Try just tempdir() too

--- tests/testthat/helper-snapshot.R
+++ tests/testthat/helper-snapshot.R
@@ -13,6 +13,7 @@ strip_dirs <- function(x) {
   x <- gsub(getwd(), "<wd>", x, fixed = TRUE)
   x <- gsub(renv_paths_cache(), "<cache>", x, fixed = TRUE)
   x <- gsub(normalizePath(tempdir()), "<tempdir>", x, fixed = TRUE)
+  x <- gsub(tempdir(), "<tempdir>", x, fixed = TRUE)
   x <- gsub("renv-library-\\w+", "<renv-library>", x)
   x <- gsub(getRversion(), "<r-version>", x, fixed = TRUE)
   x <- gsub(renv_tests_repopath(), "<test-repo>", x, fixed = TRUE)
