1e15941780cedb1de822c7f58c12f3dcda6782e2
Kevin Ushey <kevinushey@gmail.com>
2023-06-09 16:10:09 -0700

    use abort for system errors

---
+++ R/abort.R
@@ -0,0 +1,29 @@
+
+abort <- function(message, ..., details = NULL, class = NULL) {
+
+  # create condition object
+  cnd <- if (is.character(message)) {
+    cnd <- list(message = cnd, details = details, ...)
+    class(cnd) <- c(class, "error", "condition")
+  } else if (inherits(message, "condition")) {
+    message
+  } else {
+    stop("internal error: abort called with unexpected message")
+  }
+
+  # signal the condition, giving calling handlers a chance to run first
+  signalCondition(cnd)
+
+  # if we get here, the condition wasn't handled -- handle printing of
+  # the error ourselves, and then stop with a fallback condition
+  if (length(cnd$details))
+    writeLines(c(cnd$details, ""))
+
+  # create the fallback, but 'dodge' the existing error handlers
+  fallback <- cnd
+  class(fallback) <- "condition"
+
+  # now throw the error
+  stop(fallback)
+
+}
