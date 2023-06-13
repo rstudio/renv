70ad868abbe73fdae169e7bf4a79dd25c9f061e8
Hadley Wickham <h.wickham@gmail.com>
2023-06-13 07:52:50 -0500

    Update test

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -12,7 +12,7 @@ test_that("renv_metadata_version_friendly gives user friendly output", {
   )
   expect_equal(
     renv_metadata_version_friendly(list(version = "1.0.0", sha = "abcd1234")),
-    "1.0.0; rstudio/renv@abcd12"
+    "1.0.0; rstudio/renv@abcd123"
   )
 
 })
