64e4a33ac049363af5d0c5b2088a7b5daef34870
Hadley Wickham <h.wickham@gmail.com>
2023-06-14 14:25:35 -0500

    Correctly capture empty library case
    Part of #353

---
+++ tests/testthat/test-project.R
@@ -0,0 +1,11 @@
+test_that("renv_project_synchronized_check() reports if no packages installed", {
+
+  renv_tests_scope("breakfast")
+  install()
+  init()
+  unlink("renv/library", recursive = TRUE)
+
+  renv_scope_options(renv.verbose = TRUE)
+  expect_snapshot(ok <- renv_project_synchronized_check())
+  expect_false(ok)
+})
