800af3437e5417c81a525c948791720fed945e49
Kevin Ushey <kevinushey@gmail.com>
2023-06-14 21:50:17 -0700

    don't use snapshot test

--- tests/testthat/test-system.R
+++ tests/testthat/test-system.R
@@ -3,16 +3,15 @@ test_that("system errors are reported as expected", {
   skip_on_cran()
   skip_on_os("windows")
 
-  expect_snapshot(
-
-    . <- renv_system_exec(
+  cnd <- catch(
+    renv_system_exec(
       command = R(),
       args = c("--vanilla", "-s", "-e", renv_shell_quote("stop('barf')")),
       quiet = FALSE
-    ),
-
-    error = TRUE
-
+    )
   )
+
+  expect_s3_class(cnd, "error")
+  expect_true(!is.null(cnd$meta))
 
 })
