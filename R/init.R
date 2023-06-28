6b09befaaba3f55e0e2c141cb45c5d247b61ef1e
Kevin Ushey <kevinushey@gmail.com>
2023-06-28 14:32:15 -0700

    replace default RStudio mirrors with PPM (#1509)
    * replace default RStudio mirrors with PPM
    
    * only update CRAN if it exists in repos
    
    * update test

--- R/init.R
+++ R/init.R
@@ -302,9 +302,23 @@ renv_init_repos <- function() {
     return(repos)
   }
 
-  # if no repository was set, use PPM
-  if (identical(repos, list(CRAN = "@CRAN@")))
-    return(config$ppm.url())
+  # otherwise, check for some common 'default' CRAN settings
+  cran <- repos[["CRAN"]]
+  if (is.character(cran) && length(cran) == 1L) {
+    cran <- sub("/*$", "", cran)
+    defaults <- c(
+      "@CRAN@",
+      "https://cloud.R-project.org",
+      "https://cran.rstudio.com",
+      "https://cran.rstudio.org"
+    )
+
+    if (tolower(cran) %in% tolower(defaults)) {
+      repos[["CRAN"]] <- config$ppm.url()
+      return(repos)
+    }
+
+  }
 
   # repos appears to have been configured separately; just use it
   repos
