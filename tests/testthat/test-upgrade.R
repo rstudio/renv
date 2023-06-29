8a364c23b9e5211e46e6c3128c8b2f7377b150e0
Kevin Ushey <kevinushey@gmail.com>
2023-06-28 20:23:28 -0700

    update upgrade test

--- tests/testthat/test-upgrade.R
+++ tests/testthat/test-upgrade.R
@@ -8,10 +8,16 @@ test_that("the version of renv in a project can be changed (upgraded)", {
 
   # with a version number
   upgrade(version = "0.5.0")
-  expect_equal(renv_activate_version("."), "32f0f78d87150a8656a99223396f844e2fac7a17")
+  expect_equal(
+    renv_activate_version("."),
+    structure("0.5.0", sha = "32f0f78d87150a8656a99223396f844e2fac7a17")
+  )
 
   # or with a sha
   upgrade(version = "5049cef8a")
-  expect_equal(renv_activate_version("."), "5049cef8a94591b802f9766a0da092780f59f7e4")
+  expect_equal(
+    renv_activate_version("."),
+    structure("0.17.3-62", sha = "5049cef8a94591b802f9766a0da092780f59f7e4")
+  )
 
 })
