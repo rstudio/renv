c64572086581d220e7e94ef1a76fbc883bec4f7c
Kevin Ushey <kevinushey@gmail.com>
2023-07-21 22:54:10 -0700

    update package sources in repair (#1575)
    * update package sources in repair
    
    * update NEWS
    
    * add test
    
    * PR feedback

--- R/repair.R
+++ R/repair.R
@@ -1,14 +1,21 @@
 
-#' Repair a project library
+#' Repair a project
 #'
-#' Repair a project library whose cache symlinks have become broken.
-#' renv will attempt to re-install the requisite packages.
+#' Use `repair()` to recover from some common issues that can occur with
+#' a project. Currently, two operations are performed:
+#'
+#' 1. Packages with broken symlinks into the cache will be re-installed.
+#'
+#' 2. Packages that were installed from sources, but appear to be from
+#'    an remote source (e.g. GitHub), will have their `DESCRIPTION` files
+#'    updated to record that remote source explicitly.
 #'
 #' @inheritParams renv-params
 #'
 #' @param lockfile The path to a lockfile (if any). When available, renv
 #'   will use the lockfile when attempting to infer the remote associated
-#'   with the inaccessible version of each missing package.
+#'   with the inaccessible version of each missing package. When `NULL`
+#'   (the default), the project lockfile will be used.
 #'
 #' @export
 repair <- function(library  = NULL,
@@ -24,13 +31,26 @@ repair <- function(library  = NULL,
   libpaths <- renv_path_normalize(library %||% renv_libpaths_all())
   library <- libpaths[[1L]]
 
+  writef(header("Library cache links"))
+  renv_repair_links(library, lockfile, project)
+  writef()
+
+  writef(header("Package sources"))
+  renv_repair_sources(library, lockfile, project)
+  writef()
+
+  invisible()
+}
+
+renv_repair_links <- function(library, lockfile, project) {
+
+
   # figure out which library paths (junction points?) appear to be broken
   paths <- list.files(library, full.names = TRUE)
   broken <- renv_file_broken(paths)
   packages <- basename(paths[broken])
   if (empty(packages)) {
-    fmt <- "- The project library has no broken links -- nothing to do."
-    writef(fmt)
+    writef("- No issues found with the project library's cache links.")
     return(invisible(packages))
   }
 
@@ -45,10 +65,94 @@ repair <- function(library  = NULL,
     library  = library,
     project  = project
   )
+
 }
 
 renv_repair_records <- function(packages, lockfile, project) {
   map(packages, function(package) {
     lockfile$Packages[[package]] %||% package
   })
+}
+
+renv_repair_sources <- function(library, lockfile, project) {
+
+  # get package description files
+  db <- installed_packages(lib.loc = library, priority = NA_character_)
+  descpaths <- with(db, file.path(LibPath, Package, "DESCRIPTION"))
+  dcfs <- map(descpaths, renv_description_read)
+  names(dcfs) <- map_chr(dcfs, `[[`, "Package")
+
+  # try to infer sources as necessary
+  inferred <- map(dcfs, renv_repair_sources_infer)
+  inferred <- filter(inferred, Negate(is.null))
+  if (length(inferred) == 0L) {
+    writef("- All installed packages appear to be from a known source.")
+    return(TRUE)
+  }
+
+  # ask used
+  renv_scope_options(renv.verbose = TRUE)
+  renv_pretty_print(
+    c(
+      "The following package(s) do not have an explicitly-declared remote source.",
+      "However, renv was available to infer remote sources from their DESCRIPTION file."
+    ),
+    sprintf("%s  [%s]", format(names(inferred)), inferred),
+    "`renv::restore()` may fail for packages without an explicitly-declared remote source."
+  )
+
+  choice <- menu(
+
+    choices =  c(
+      update = "Let renv infer the remote sources for these packages.",
+      cancel = "Do nothing and resolve the situation another way."
+    ),
+
+    title = "What would you like to do?"
+
+  )
+
+  cancel_if(identical(choice, "cancel"))
+
+  enumerate(inferred, function(package, remote) {
+    record <- renv_remotes_resolve(remote)
+    record[["RemoteSha"]] <- NULL
+    renv_package_augment(file.path(library, package), record)
+  })
+
+  n <- length(inferred)
+  writef("- Updated %i package DESCRIPTION %s.", n, nplural("file", n))
+
+  TRUE
+
+}
+
+renv_repair_sources_infer <- function(dcf) {
+
+  # if this package appears to have a declared remote, use as-is
+  for (field in c("RemoteType", "Repository", "biocViews"))
+    if (!is.null(dcf[[field]]))
+      return(NULL)
+
+  # ok, this is a package installed from sources that "looks" like
+  # the development version of a package; try to guess its remote
+  guess <- function(pattern, field) {
+    urls <- strsplit(dcf[[field]] %||% "", "\\s*,\\s*")[[1L]]
+    for (url in urls) {
+      matches <- regmatches(url, regexec(pattern, url, perl = TRUE))[[1L]]
+      if (length(matches) == 3L)
+        return(paste(matches[[2L]], matches[[3L]], sep = "/"))
+    }
+  }
+
+  # first, check bug reports
+  remote <- guess("^https://(?:www\\.)?github\\.com/([^/]+)/([^/]+)/issues$", "BugReports")
+  if (!is.null(remote))
+    return(remote)
+
+  # next, check the URL field
+  remote <- guess("^https://(?:www\\.)?github\\.com/([^/]+)/([^/]+)", "URL")
+  if (!is.null(remote))
+    return(remote)
+
 }
