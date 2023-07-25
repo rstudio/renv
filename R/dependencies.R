70b25b932089f24640c1f3d07c56721a73b8818b
Kevin Ushey <kevinushey@gmail.com>
2023-07-25 12:13:09 -0700

    add some operators

--- R/dependencies.R
+++ R/dependencies.R
@@ -1709,8 +1709,18 @@ renv_dependencies_report <- function(errors) {
 
 renv_dependencies_eval <- function(expr) {
 
-  # create environment with small subset of symbols
-  syms <- c("list", "c", "!", "+", "-", "*", "/", "||", "&&")
+  # create environment with small subset of "safe" symbols, that
+  # are commonly used for chunk expressions
+  syms <- c(
+    "list", "c",
+    "::", ":::", "$", "@", "[", "[[",
+    ":",
+    "+", "-", "*", "/",
+    "<", ">", "<=", ">=", "==", "!=",
+    "!",
+    "&", "&&", "|", "||"
+  )
+
   vals <- mget(syms, envir = baseenv())
   envir <- list2env(vals, parent = emptyenv())
 
