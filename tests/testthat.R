0e780cba5aa8d5b8c1d3bce9c9233e0349929eb6
Kevin Ushey <kevinushey@gmail.com>
2024-02-28 21:20:19 -0800

    Release/1.0.5 (#1836)
    * CRAN release 1.0.5
    
    * initialize cache directory when running tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,16 @@
 
+library(tools)
 library(testthat)
 library(renv, warn.conflicts = FALSE)
 
 if (!renv:::renv_tests_supported()) {
   message("* renv does not support running tests on this platform.")
   if (!interactive()) quit(status = 0L)
+}
+
+if (Sys.info()[["sysname"]] == "Linux") {
+  cachedir <- R_user_dir("renv", "cache")
+  dir.create(cachedir, recursive = TRUE, showWarnings = FALSE)
 }
 
 test_check("renv")
