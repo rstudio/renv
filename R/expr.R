17efb74341637d36634e6bd0a808981955b86677
Kevin Ushey <kevinushey@gmail.com>
2024-03-09 14:23:49 -0800

    comment

--- R/expr.R
+++ R/expr.R
@@ -1,4 +1,15 @@
 
+# TODO: This performs rlang-style injection, but doesn't
+# fix up the parse tree as rlang does -- since calls like
+#
+#    !!a + !!b
+#
+# is handled by rlang as though it were parsed as
+#
+#    `+`(!!a, !!b)
+#
+# but this is not how the regular R precedence rules parse
+# these expressions. Should we just use bquote()?
 expr <- function(expr, envir = parent.frame()) {
   renv_expr_impl(substitute(expr), envir)
 }
