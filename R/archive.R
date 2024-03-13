e62301ca60692f22e07471cdfc5ab21e89d9fbde
Kevin Ushey <kevinushey@gmail.com>
2024-03-13 16:42:17 -0700

    remove leading './' when listing archive files (#1852)

--- R/archive.R
+++ R/archive.R
@@ -13,7 +13,21 @@ renv_archive_type <- function(archive) {
 }
 
 renv_archive_list <- function(archive) {
-  suppressWarnings(renv_archive_list_impl(archive))
+
+  # list files
+  files <- suppressWarnings(renv_archive_list_impl(archive))
+
+  # remove a leading './' if included on the paths
+  # TODO: do we need to perform any other sort of path canonicalization?
+  #
+  # https://github.com/rstudio/renv/issues/1852
+  idx <- regexpr("./", files, fixed = TRUE)
+  matches <- idx == 1L
+  files[matches] <- substring(files[matches], 3L)
+
+  # return formatted files
+  files
+
 }
 
 renv_archive_list_impl <- function(archive) {
