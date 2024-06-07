a11676f67491c2b27e4032fb2d71e627e5237321
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-06-07 13:26:43 -0700

    Make `renv::restore()` (maybe) a runnable hyperlink if cli is available

--- R/load.R
+++ R/load.R
@@ -884,7 +884,13 @@ renv_load_report_synchronized <- function(project = NULL, lockfile = NULL) {
     caution("- None of the packages recorded in the lockfile are currently installed.")
     autoloading <- getOption("renv.autoloader.running", default = FALSE)
     if (autoloading) {
-      caution("- Use `renv::restore()` to restore the project library.")
+      if (requireNamespace("cli", quietly = TRUE)) {
+        cli::cli_bullets(c(
+          "*" = "Use {.run renv::restore()} to restore the project library."
+        ))
+      } else {
+        caution("- Use `renv::restore()` to restore the project library.")
+      }
       return(FALSE)
     }
 
